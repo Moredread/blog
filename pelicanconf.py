de9ef2fa8bf53da096f30964f1f8acab3c8857fa
Andre-Patrick Bubel <code@andre-bubel.de>
2015-08-31 22:43:19 +0200

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,18 +9,24 @@ SITEURL = ''
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
-PATH = 'content'
+PATH = 'blog'
 
 TIMEZONE = 'Europe/Berlin'
 
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+#FEED_ALL_ATOM = None
+#CATEGORY_FEED_ATOM = None
+#TRANSLATION_FEED_ATOM = None
+#AUTHOR_FEED_ATOM = None
+#AUTHOR_FEED_RSS = None
+
+FEED_ALL_ATOM = 'atom.xml'
+CATEGORY_FEED_ATOM = '%s.atom.xml'
+
+FEED_ALL_RSS = 'rss.xml'
+CATEGORY_FEED_RSS = '%s.rss.xml'
 
 # Blogroll
 #LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -32,9 +38,16 @@ AUTHOR_FEED_RSS = None
 #SOCIAL = (('You can add links in your config file', '#'),
 #          ('Another social link', '#'),)
 
+SOCIAL = (('feed', 'atom.xml', 'rss'),
+          ('twitter', 'http://twitter.com/IndustrialRobot'),
+          ('github', 'http://github.com/Moredread'),
+         )
+
 DIRECT_TEMPLATES = ['index', 'archives']
 
 STATIC_PATHS = ['images', 'static']
+
+THEME = "themes/pelican-bootstrap3"
 
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
@@ -42,26 +55,39 @@ ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 PLUGIN_PATHS = ['plugins']
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
-           'liquid_tags.include_code', 'image_process']
+           'liquid_tags.include_code', 'image_process',
+           'tag_cloud']
+
+TAG_CLOUD_STEPS = 4
+TAG_CLOUD_MAX_ITEMS = 20
+TAG_CLOUD_SORTING = 'alphabetically'
 
 #FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
 #SLUGIFY_SOURCE = 'basename'
 
-MENUITEMS = [
-                ("Archives", "archives.html"),
-            ]
+#MENUITEMS = [
+#                ("Archives", "archives.html"),
+#            ]
+
+DISPLAY_CATEGORIES_ON_MENU = False
 
 YEAR_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'archives']
 
-IMAGE_PROCESS = {
-    'crisp': {'type': 'responsive-image',
-              'srcset': [('1x', ["scale_in 800 600 True"]),
-                         ('2x', ["scale_in 1600 1200 True"]),
-                         ('4x', ["scale_in 3200 2400 True"]),
-                         ],
-               'default': '1x',
-             },
-    }
+#DISPLAY_TAGS_ON_SIDEBAR = False
+#DISPLAY_TAGS_INLINE = True
+CC_LICENSE = "CC-BY-NC-SA"
+
+# IMAGE_PROCESS = {
+#     'crisp': {'type': 'responsive-image',
+#               'srcset': [('1x', ["scale_in 800 600 True"]),
+#                          ('2x', ["scale_in 1600 1200 True"]),
+#                          ('4x', ["scale_in 3200 2400 True"]),
+#                          ],
+#                'default': '1x',
+#              },
+#     }
 
 DEFAULT_CATEGORY = "Blog"
 DEFAULT_PAGINATION = 20
